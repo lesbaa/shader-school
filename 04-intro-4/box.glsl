ed5778808385689cfdc582275f15ba99ac52775d
Les Moffat <les.moffat@stv.tv>
2018-04-20 13:37:50 +0100

    more practice

--- 04-intro-4/box.glsl
+++ 04-intro-4/box.glsl
@@ -1,8 +1,11 @@
 bool inBox(highp vec2 lo, highp vec2 hi, highp vec2 p) {
 
   //Test if the point p is inside the box bounded by [lo, hi]
-
-  return false;
+  bool isBelow = all(lessThanEqual(hi));
+  bool isAbove = all(greaterThanEqual(lo));
+  
+  return isBelow && isAbove;
+  
 }
 
 
